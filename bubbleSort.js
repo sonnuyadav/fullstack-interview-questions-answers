d192393c179adc52150eed4b281b86cc749befbf
sonnuyadav <sonnu.yadav@gmail.com>
2023-10-13 20:49:07 +0530

    Added bubblesort

---
+++ bubbleSort.js
@@ -0,0 +1,24 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    let swapped;
+  
+    do {
+      swapped = false;
+      for (let i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          // Swap the elements
+          const temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+  
+    return arr;
+  }
+  
+  // Example usage:
+  const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+  const sortedArray = bubbleSort(unsortedArray);
+  console.log(sortedArray);

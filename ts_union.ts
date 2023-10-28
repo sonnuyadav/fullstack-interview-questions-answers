5960a34ea3a02b81151fc93ea51d5821e48dc7dc
Sonu Yadav <sonnu.yadav@gmail.com>
2023-10-28 23:35:52 +0530

    added union in typescript

---
+++ ts_union.ts
@@ -0,0 +1,16 @@
+let score: number | string = 33
+score = 44
+score = "45"
+
+
+type User = {
+    name: string;
+    id: number
+}
+
+const data : number[] = [1,2,3]
+const data2 : string[] = ["1", "2", "3"]
+const data3 : (string | number | boolean)[] = ["1", 2, true]
+
+
+

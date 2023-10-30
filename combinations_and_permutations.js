e02b1270914c506465f9fb55729b81981e01550c
Sonu Yadav <sonnu.yadav@gmail.com>
2023-10-30 23:26:26 +0530

    string to array

--- combinations_and_permutations.js
+++ combinations_and_permutations.js
@@ -1,8 +1,7 @@
 
 //['ab', 'ac', 'ad','ba', 'bc', 'bd','ca', 'cb', 'cd','da', 'db', 'dc']
 const str = 'abcd';
-const characters = ["a", "b", "c", "d"];
-
+const characters = str.split("");
 const combinations = (arr, min = 1, max) => {
   const combination = (arr, depth) => {
     if (depth === 1) {
@@ -19,6 +18,5 @@ const combinations = (arr, min = 1, max) => {
 };
 
 const result = combinations(characters, 2, 2);
-
 console.log(`Combinations: ${result.length}`, result);
 //output ['aa','ab','ac','ad','ba','bb','bc','bd','ca','cb','cc','cd','da','db','dc','dd','aaa','aab','aac','aad','aba','abb','abc','abd']

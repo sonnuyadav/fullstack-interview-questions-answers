65762f492be4c5d49a1600ae5b67869ec6607a5d
sonnuyadav <sonnu.yadav@gmail.com>
2023-12-08 17:26:37 +0530

    Write a javascript program to find the most frequent item in an array

---
+++ find_most_frequent_item_in_array.js
@@ -0,0 +1,21 @@
+//Write a javascript program to find the most frequent item in an array
+var mostFrequnet = null,mostFrequnetItem ;
+var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+arr1.sort();
+
+
+for (var i=0;i<arr1.length;i++){ 
+
+     var single = arr1[i]; 
+     var total = (arr1.lastIndexOf(single)-arr1.indexOf(single))+1; 
+
+     if(total > mostFrequnet) {
+
+         mostFrequnetItem = arr1[i];
+         mostFrequnet = total;
+         i= arr1.lastIndexOf(single)+1;
+
+    }
+}
+
+console.log(mostFrequnetItem + " ( "+mostFrequnet+" times )");

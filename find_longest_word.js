ef3590d8b3d7c093daf1b0e01830d0a46ef9aedb
Sonu Yadav <sonnu.yadav@gmail.com>
2024-01-10 23:38:52 +0530

    find the longest word in a given sentence

--- find_longest_word.js
+++ find_longest_word.js
@@ -1,5 +1,5 @@
-
+// find the longest word in a given sentence
 const longestWord = (sentence) => sentence.split(' ').reduce((longest, word) => word.length > longest.length ? word : longest, '');
 
 const inputSentence = 'This is a animal';
-console.log(longestWord(inputSentence));
+console.log(longestWord(inputSentence)); //animal

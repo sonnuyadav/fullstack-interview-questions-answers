14fafcafd3337aee917f9b1812d446b12d0dcb9b
Sonu Yadav <sonnu.yadav@gmail.com>
2024-01-10 23:32:50 +0530

    Added countArrayOccranceValue

---
+++ countArrayOccranceValue.js
@@ -0,0 +1,6 @@
+const countOccurrences = (arr) => arr.reduce((acc, val) => (acc[val] = (acc[val] || 0) + 1, acc), {});
+
+//const inputArray = [1, 2, 2, 3, 4, 4, 4, 5];
+const inputArray = ["sonu", "ramesh", "sonu", "Monu", 'ramesh'];
+const result = countOccurrences(inputArray);
+console.log(result);

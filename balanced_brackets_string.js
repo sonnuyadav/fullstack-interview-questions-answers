438c05d90907e9e3e59a717dbb8aa55611ad2e73
Sonu Yadav <sonnu.yadav@gmail.com>
2024-01-12 23:13:28 +0530

    valid parentheses

--- balanced_brackets_string.js
+++ balanced_brackets_string.js
@@ -1,6 +1,24 @@
-
-const areBracketsBalanced = (str) => !str.split('').reduce((count, char) => (char === '(' || char === '[' || char === '{') ? ++count : (char === ')' || char === ']' || char === '}') ? --count : count, 0);
-
-const s1 = '({[]}{})';
-const r1 = areBracketsBalanced(s1); // r1: true
-console.log(r1);
+function isValidParentheses(s) {
+    const stack = [];
+    const openingBrackets = ['(', '{', '['];
+    const closingBrackets = [')', '}', ']'];
+    
+    for (let i = 0; i < s.length; i++) {
+      const currentBracket = s[i];
+      if (openingBrackets.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else if (closingBrackets.includes(currentBracket)) {
+        const lastOpeningBracket = stack.pop();
+        if (!lastOpeningBracket || 
+            openingBrackets.indexOf(lastOpeningBracket) !== closingBrackets.indexOf(currentBracket)) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+  
+  const validString = '({[]}{})';
+  const inValidString = "}}{{";
+  console.log(isValidParentheses(validString)); // Output: true
+  

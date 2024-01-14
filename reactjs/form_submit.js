cb3556ac6b88295c92db9cbbd3ef8da52fdc1a97
sonnuyadav <sonnu.yadav@gmail.com>
2024-01-14 18:48:53 +0530

    added functional and class component

---
+++ reactjs/form_submit.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const MyForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform actions with the form data
+    console.log('Form submitted:', formData);
+    // Reset the form after submission
+    setFormData({
+      firstName: '',
+      lastName: '',
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default MyForm;

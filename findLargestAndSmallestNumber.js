ff1f581db7e30fefba930c17d71b82d599f8b4c2
sonnuyadav <sonnu.yadav@gmail.com>
2024-01-23 18:24:56 +0530

    convert string to number

--- findLargestAndSmallestNumber.js
+++ findLargestAndSmallestNumber.js
@@ -1,13 +1,14 @@
-const arr = [1, 2, 3, 4, 100];
+const arr = [1,2,3,4,5,6,100];
+
 const findMaxMin = (arr) => {
   let max = arr[0];
   let min = arr[0];
   
   for(let i = 0; i < arr.length; i++) {
     if(arr[i] > max) {
-      max = arr[i];
+      max = Number(arr[i]);
     } else if (arr[i] < min) {
-      min = arr[i];
+      min = Number(arr[i]);
     }
   }
   

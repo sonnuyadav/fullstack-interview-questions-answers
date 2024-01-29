90e63f22fdd6018a5429a1d5bd094df7151974ba
Sonu Yadav <sonnu.yadav@gmail.com>
2024-01-29 23:46:06 +0530

    added largest element

---
+++ largest_element_nested_array.js
@@ -0,0 +1,30 @@
+function findLargestInNestedArray(arr) {
+    let largestElement = Number.NEGATIVE_INFINITY;
+    function traverseArray(currentArray) {
+      for (let i = 0; i < currentArray.length; i++) {
+        const currentElement = currentArray[i];
+        if (Array.isArray(currentElement)) {
+          traverseArray(currentElement);
+        } else {
+          // Update the largestElement if the current element is greater
+          if (currentElement > largestElement) {
+            largestElement = currentElement;
+          }
+        }
+      }
+    }
+  
+    traverseArray(arr);
+    return largestElement;
+  }
+  
+  // Example usage:
+  const nestedArray = [
+    [1, 2, 3],
+    [4, [5, 6], 7],
+    [8, 9, 10]
+  ];
+  
+  const largestElement = findLargestInNestedArray(nestedArray);
+  console.log("The largest element in the nested array is:", largestElement);
+  

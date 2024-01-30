225c20a0fbf271663d6c857da3f161825ef368ee
sonnuyadav <sonnu.yadav@gmail.com>
2024-01-30 14:30:23 +0530

    change the possition of .reduce

--- DuplicateRemove.js
+++ DuplicateRemove.js
@@ -1,5 +1,16 @@
+// #1 remove duplicate using .reduce method
+const array = [1, 2, 3, 4, 1, 2, 3];
+const uniqueArray = array.reduce((accumulator, currentValue) => {
+  if (!accumulator.includes(currentValue)) {
+    accumulator.push(currentValue);
+  }
+  return accumulator;
+}, []);
+console.log(uniqueArray);
+
 var arr = [1,3,3,3,1,5,6,7,8,1];
 
+//#2
 const removeDuplicate = (arr) =>{
     var exists ={},
       outArr = [], 
@@ -17,22 +28,12 @@ const removeDuplicate = (arr) =>{
 
 console.log(removeDuplicate(arr));
 
-//Another way with ES6
+//#3 with ES6
 const filteredArray = arr.filter((value, index, array) => 
   array.indexOf(value) === index
 );
 
 console.log(filteredArray); // [ 1, 3, 5, 6, 7, 8 ]
 
-//third way
+//#4
 console.log([...new Set(arr)]);
-
-// remove duplicate using .reduce method
-const array = [1, 2, 3, 4, 1, 2, 3];
-const uniqueArray = array.reduce((accumulator, currentValue) => {
-  if (!accumulator.includes(currentValue)) {
-    accumulator.push(currentValue);
-  }
-  return accumulator;
-}, []);
-console.log(uniqueArray);
